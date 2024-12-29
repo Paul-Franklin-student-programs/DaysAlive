622c77eabaede683a20f2d72142873f712cbac74
Paul-Franklin-student-programs <pfranklin138@gmail.com>
2024-12-29 15:24:37 -0500

    first successful test in emulator

--- app/src/main/java/MainActivity.kt
+++ app/src/main/java/MainActivity.kt
@@ -1,10 +1,11 @@
+package com.example.daysalive
+
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.example.daysalive.R
 
 class MainActivity : AppCompatActivity() {
 
