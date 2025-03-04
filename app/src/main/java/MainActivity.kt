3b96bdc4234622cd064709d97aa965bbab1f4591
Paul-Franklin-student-programs <pfranklin138@gmail.com>
2025-03-04 16:25:11 -0500

    adding logic to handle age-related images

--- app/src/main/java/MainActivity.kt
+++ app/src/main/java/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import java.text.NumberFormat
@@ -17,10 +18,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+
+
         // Access UI elements
         val tryAgainButton = findViewById<TextView>(R.id.btn_try_again)
-        /*val titleImage = findViewById<ImageView>(R.id.myTitle)
-        val logoImage = findViewById<ImageView>(R.id.myLogo)*/
         val promptTextView = findViewById<TextView>(R.id.et_prompt)
         val monthField = findViewById<EditText>(R.id.et_month)
         val dayField = findViewById<EditText>(R.id.et_day)
@@ -29,6 +30,35 @@ class MainActivity : AppCompatActivity() {
         val resultTextViewYears = findViewById<TextView>(R.id.tv_result)
         val resultTextViewMessage = findViewById<TextView>(R.id.tv_result_two)
 
+        // Age-Specific Images
+        val age_0_9: ImageView = findViewById(R.id.age_0_9);
+        val age_10_19: ImageView = findViewById(R.id.age_10_19)
+        val age_20_29: ImageView = findViewById(R.id.age_20_29)
+        val age_30_39: ImageView = findViewById(R.id.age_30_39)
+        val age_40_49: ImageView = findViewById(R.id.age_40_49)
+        val age_50_59: ImageView = findViewById(R.id.age_50_59)
+        val age_60_69: ImageView = findViewById(R.id.age_60_69)
+        val age_70_79: ImageView = findViewById(R.id.age_70_79)
+        val age_80_89: ImageView = findViewById(R.id.age_80_89)
+        val age_90_99: ImageView = findViewById(R.id.age_90_99)
+        val age_100: ImageView = findViewById(R.id.age_100)
+
+        fun determineImage(daysAlive: Long) {
+            when (daysAlive) {
+                in 0..3652 -> age_0_9.visibility = View.VISIBLE
+                in 3653..7304 -> age_10_19.visibility = View.VISIBLE
+                in 7305..10957 -> age_20_29.visibility = View.VISIBLE
+                in 10958..14609 -> age_30_39.visibility = View.VISIBLE
+                in 14610..18262 -> age_40_49.visibility = View.VISIBLE
+                in 18263..21914 -> age_50_59.visibility = View.VISIBLE
+                in 21915..25567 -> age_60_69.visibility = View.VISIBLE
+                in 25568..29219 -> age_70_79.visibility = View.VISIBLE
+                in 29220..32872 -> age_80_89.visibility = View.VISIBLE
+                in 32873..36524 -> age_90_99.visibility = View.VISIBLE
+                else -> age_100.visibility = View.VISIBLE
+            }
+        }
+
         calculateButton.setOnClickListener {
 
             val month: Int = monthField.text.toString().toInt()
@@ -36,7 +66,8 @@ class MainActivity : AppCompatActivity() {
             val year: Int = yearField.text.toString().toInt()
 
             val dayCountRaw: Long = Calculations.birthdayCalc(day, month, year)
-            val dayCountStyled: String = NumberFormat.getNumberInstance(Locale.US).format(dayCountRaw)
+            val dayCountStyled: String =
+                NumberFormat.getNumberInstance(Locale.US).format(dayCountRaw)
             val dayCountStyledFinal = "You have been alive for... \n\n$dayCountStyled days!"
             val daysAliveMessage: String = getLifeStageMessage(dayCountRaw)
 
@@ -52,9 +83,12 @@ class MainActivity : AppCompatActivity() {
             resultTextViewYears.visibility = View.VISIBLE
             resultTextViewMessage.text = daysAliveMessage
             resultTextViewMessage.visibility = View.VISIBLE
+
+            determineImage(dayCountRaw)
         }
 
         tryAgainButton.setOnClickListener {
+
             promptTextView.visibility = View.VISIBLE
             monthField.visibility = View.VISIBLE
             yearField.visibility = View.VISIBLE
@@ -66,7 +100,15 @@ class MainActivity : AppCompatActivity() {
             resultTextViewYears.visibility = View.GONE
             resultTextViewMessage.visibility = View.GONE
         }
-        }
+
+
+
+
+
+    }
 }
+
+
+
 
 
