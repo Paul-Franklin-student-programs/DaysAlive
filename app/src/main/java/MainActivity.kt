39f03a1dad75b1d91d71032f054c5feaebf9aecd
Paul-Franklin-student-programs <pfranklin138@gmail.com>
2025-03-09 17:02:44 -0400

    made improvements to UI by using less distracting background colors on the main screen

--- app/src/main/java/MainActivity.kt
+++ app/src/main/java/MainActivity.kt
@@ -47,10 +47,6 @@ class MainActivity : AppCompatActivity() {
         myLogo.imageMatrix = matrix2
 
 
-
-
-
-
         // Access UI elements
         val tryAgainButton = findViewById<TextView>(R.id.btn_try_again)
         val promptTextView = findViewById<TextView>(R.id.et_prompt)
@@ -77,17 +73,17 @@ class MainActivity : AppCompatActivity() {
 
 
         // Age-Specific Images
-            val age0 = findViewById<ImageView>(R.id.age_0_9)
-            val age10 = findViewById<ImageView>(R.id.age_10_19)
-            val age20 = findViewById<ImageView>(R.id.age_20_29)
-            val age30 = findViewById<ImageView>(R.id.age_30_39)
-            val age40 = findViewById<ImageView>(R.id.age_40_49)
-            val age50 = findViewById<ImageView>(R.id.age_50_59)
-            val age60 = findViewById<ImageView>(R.id.age_60_69)
-            val age70 = findViewById<ImageView>(R.id.age_70_79)
-            val age80 = findViewById<ImageView>(R.id.age_80_89)
-            val age90 = findViewById<ImageView>(R.id.age_90_99)
-            val age100 = findViewById<ImageView>(R.id.age_100)
+        val age0 = findViewById<ImageView>(R.id.age_0_9)
+        val age10 = findViewById<ImageView>(R.id.age_10_19)
+        val age20 = findViewById<ImageView>(R.id.age_20_29)
+        val age30 = findViewById<ImageView>(R.id.age_30_39)
+        val age40 = findViewById<ImageView>(R.id.age_40_49)
+        val age50 = findViewById<ImageView>(R.id.age_50_59)
+        val age60 = findViewById<ImageView>(R.id.age_60_69)
+        val age70 = findViewById<ImageView>(R.id.age_70_79)
+        val age80 = findViewById<ImageView>(R.id.age_80_89)
+        val age90 = findViewById<ImageView>(R.id.age_90_99)
+        val age100 = findViewById<ImageView>(R.id.age_100)
 
         calculateButton.isSoundEffectsEnabled = false
         tryAgainButton.isSoundEffectsEnabled = false
@@ -107,7 +103,7 @@ class MainActivity : AppCompatActivity() {
                 in 7305..10957 -> age20.visibility = View.VISIBLE
                 in 10958..14609 -> age30.visibility = View.VISIBLE
                 in 14610..18262 -> age40.visibility = View.VISIBLE
-                in 18263..21914 ->age50.visibility = View.VISIBLE
+                in 18263..21914 -> age50.visibility = View.VISIBLE
                 in 21915..25567 -> age60.visibility = View.VISIBLE
                 in 25568..29219 -> age70.visibility = View.VISIBLE
                 in 29220..32872 -> age80.visibility = View.VISIBLE
@@ -193,174 +189,167 @@ class MainActivity : AppCompatActivity() {
         }
 
 
-        val paperCup = View.OnClickListener {
-            try {
-
-                val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-                inputMethodManager.hideSoftInputFromWindow(window.decorView.windowToken, 0)
 
 
-                planetMars.visibility = View.GONE
-                planetVenus.visibility = View.GONE
-                planetMercury.visibility = View.GONE
-                planetTheMoon.visibility = View.GONE
-                planetTheSun.visibility = View.GONE
-                planetMercury.visibility = View.GONE
-                planetVenus.visibility = View.GONE
-                planetPluto.visibility = View.GONE
-                planetJupiter.visibility = View.GONE
-                planetSaturn.visibility = View.GONE
-                planetUranus.visibility = View.GONE
-                planetNeptune.visibility = View.GONE
+                val paperCup = View.OnClickListener {
+                    try {
+                        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                        inputMethodManager.hideSoftInputFromWindow(window.decorView.windowToken, 0)
 
-                firstZodiacMessage.visibility = View.GONE
-                secondZodiacMessage.visibility = View.GONE
+                        // Hide unnecessary elements
+                        planetMars.visibility = View.GONE
+                        planetVenus.visibility = View.GONE
+                        planetMercury.visibility = View.GONE
+                        planetTheMoon.visibility = View.GONE
+                        planetTheSun.visibility = View.GONE
+                        planetPluto.visibility = View.GONE
+                        planetJupiter.visibility = View.GONE
+                        planetSaturn.visibility = View.GONE
+                        planetUranus.visibility = View.GONE
+                        planetNeptune.visibility = View.GONE
+                        firstZodiacMessage.visibility = View.GONE
+                        secondZodiacMessage.visibility = View.GONE
+                        errorTextViewMessage.visibility = View.GONE
 
+                        // Get user input
+                        val month = monthField.text.toString()
+                        val day = dayField.text.toString()
+                        val year = yearField.text.toString()
 
+                        // Call birthdayCalc and store the result
+                        val result = Calculations.birthdayCalc(day, month, year)
 
+                        if (result is String) {
+                            // If result is an error message, display it
+                            playSound(R.raw.dayalive_sound_wrong)
+                            errorTextViewMessage.text = result
+                            errorTextViewMessage.visibility = View.VISIBLE
+                            return@OnClickListener // Stop execution here
+                        }
 
+                        // Ensure result is a valid number before casting
+                        if (result !is Long) {
+                            throw IllegalStateException("Unexpected result type from birthdayCalc()")
+                        }
 
-                errorTextViewMessage.visibility = View.GONE
-                firstZodiacMessage.visibility = View.GONE
-                secondZodiacMessage.visibility = View.GONE
+                        val dayCountRaw = result
+                        val dayCountStyled = NumberFormat.getNumberInstance(Locale.US).format(dayCountRaw)
+                        val daysAliveMessage = getLifeStageMessage(dayCountRaw)
 
-                // Get user input
-                val month = monthField.text.toString()
-                val day = dayField.text.toString()
-                val year = yearField.text.toString()
+                        dayCountStyledFinal = "You have been alive for... \n$dayCountStyled days!"
 
-                // Call birthdayCalc and store the result
-                val result = Calculations.birthdayCalc(day, month, year)
+                        if (promptTextView.visibility == View.VISIBLE) {
+                            playSound(R.raw.daysalive_sound_correct)
+                        }
 
-                if (result is String) {
-                    // If result is an error message, display it
-                    playSound(R.raw.dayalive_sound_wrong)
-                    errorTextViewMessage.text = result
-                    errorTextViewMessage.visibility = View.VISIBLE
+                        // Hide input fields and show results
+                        promptTextView.visibility = View.GONE
+                        monthField.visibility = View.GONE
+                        yearField.visibility = View.GONE
+                        dayField.visibility = View.GONE
+                        calculateButton.visibility = View.GONE
 
+                        resultTextViewYears.text = dayCountStyledFinal
+                        resultTextViewYears.visibility = View.VISIBLE
+
+                        resultTextViewMessage.text = daysAliveMessage
+                        resultTextViewMessage.visibility = View.VISIBLE
+
+                        determineImage(dayCountRaw)
+                        tryAgainButton.visibility = View.VISIBLE
+
+                    } catch (e: Exception) {
+                        playSound(R.raw.dayalive_sound_wrong)
+
+                        // Handle specific errors first
+                        when (e) {
+                            is NumberFormatException, is DateTimeException -> {
+                                errorTextViewMessage.text = "Error: The date you entered does not exist!"
+                            }
+                            else -> {
+                                errorTextViewMessage.text = "An unexpected error occurred.\nPlease try again."
+                            }
+                        }
+
+                        errorTextViewMessage.visibility = View.VISIBLE
+                    }
                 }
 
+                calculateButton.setOnClickListener(paperCup)
+                planetMars.setOnClickListener(paperCup)
+                planetMercury.setOnClickListener(paperCup)
+                planetNeptune.setOnClickListener(paperCup)
+                planetPluto.setOnClickListener(paperCup)
+                planetSaturn.setOnClickListener(paperCup)
+                planetTheMoon.setOnClickListener(paperCup)
+                planetTheSun.setOnClickListener(paperCup)
+                planetUranus.setOnClickListener(paperCup)
+                planetVenus.setOnClickListener(paperCup)
+
+                age0.setOnClickListener(glassBowl)
+                age10.setOnClickListener(glassBowl)
+                age20.setOnClickListener(glassBowl)
+                age30.setOnClickListener(glassBowl)
+                age40.setOnClickListener(glassBowl)
+                age50.setOnClickListener(glassBowl)
+                age60.setOnClickListener(glassBowl)
+                age70.setOnClickListener(glassBowl)
+                age80.setOnClickListener(glassBowl)
+                age90.setOnClickListener(glassBowl)
+                age100.setOnClickListener(glassBowl)
 
 
-                    val dayCountRaw = result as Long
-                    val dayCountStyled = NumberFormat.getNumberInstance(Locale.US).format(dayCountRaw)
-                    val daysAliveMessage = getLifeStageMessage(dayCountRaw)
-                // If result is a valid day count, continue
-                dayCountStyledFinal =
-                "You have been alive for... \n$dayCountStyled days!"
 
-                if (promptTextView.visibility == View.VISIBLE) {
-                    playSound(R.raw.daysalive_sound_correct)
+
+
+
+
+                tryAgainButton.setOnClickListener {
+
+                    firstZodiacMessage.visibility = View.GONE
+                    secondZodiacMessage.visibility = View.GONE
+                    playSound(R.raw.daysalive_sound_return)
+                    // Reset input fields
+                    monthField.text.clear()
+                    yearField.text.clear()
+                    dayField.text.clear()
+
+                    // Restore visibility
+                    promptTextView.visibility = View.VISIBLE
+                    monthField.visibility = View.VISIBLE
+                    yearField.visibility = View.VISIBLE
+                    dayField.visibility = View.VISIBLE
+                    calculateButton.visibility = View.VISIBLE
+
+                    tryAgainButton.visibility = View.GONE
+                    resultTextViewYears.visibility = View.GONE
+                    resultTextViewMessage.visibility = View.GONE
+
+                    planetMars.visibility = View.GONE
+                    planetVenus.visibility = View.GONE
+                    planetMercury.visibility = View.GONE
+                    planetTheMoon.visibility = View.GONE
+                    planetTheSun.visibility = View.GONE
+                    planetMercury.visibility = View.GONE
+                    planetVenus.visibility = View.GONE
+                    planetPluto.visibility = View.GONE
+                    planetJupiter.visibility = View.GONE
+                    planetSaturn.visibility = View.GONE
+                    planetUranus.visibility = View.GONE
+                    planetNeptune.visibility = View.GONE
+
+                    age0.visibility = View.GONE
+                    age10.visibility = View.GONE
+                    age20.visibility = View.GONE
+                    age30.visibility = View.GONE
+                    age40.visibility = View.GONE
+                    age50.visibility = View.GONE
+                    age60.visibility = View.GONE
+                    age70.visibility = View.GONE
+                    age80.visibility = View.GONE
+                    age90.visibility = View.GONE
+                    age100.visibility = View.GONE
+
                 }
-
-                // Hide input fields and show results
-                promptTextView.visibility = View.GONE
-                monthField.visibility = View.GONE
-                yearField.visibility = View.GONE
-                dayField.visibility = View.GONE
-                calculateButton.visibility = View.GONE
-
-                resultTextViewYears.text = dayCountStyledFinal
-                resultTextViewYears.visibility = View.VISIBLE
-
-
-
-                resultTextViewMessage.text = daysAliveMessage
-                resultTextViewMessage.visibility = View.VISIBLE
-
-                determineImage(dayCountRaw)
-                tryAgainButton.visibility = View.VISIBLE
-
-            } catch (e: Exception) {
-                if (e is NumberFormatException || e is DateTimeException) {
-                    playSound(R.raw.dayalive_sound_wrong)
-                    // If error is related to parsing or date validation, show the correct message
-                    errorTextViewMessage.text = "Error: The date you entered does not exist!"
-                } else {
-                    playSound(R.raw.dayalive_sound_wrong)
-                    // For truly unexpected errors, show general error message
-                    errorTextViewMessage.text = "An unexpected error occurred.\nPlease try again."
-                }
-                errorTextViewMessage.visibility = View.VISIBLE
             }
-
         }
-        calculateButton.setOnClickListener(paperCup)
-        planetMars.setOnClickListener(paperCup)
-        planetMercury.setOnClickListener(paperCup)
-        planetNeptune.setOnClickListener(paperCup)
-        planetPluto.setOnClickListener(paperCup)
-        planetSaturn.setOnClickListener(paperCup)
-        planetTheMoon.setOnClickListener(paperCup)
-        planetTheSun.setOnClickListener(paperCup)
-        planetUranus.setOnClickListener(paperCup)
-        planetVenus.setOnClickListener(paperCup)
-
-        age0.setOnClickListener(glassBowl)
-        age10.setOnClickListener(glassBowl)
-        age20.setOnClickListener(glassBowl)
-        age30.setOnClickListener(glassBowl)
-        age40.setOnClickListener(glassBowl)
-        age50.setOnClickListener(glassBowl)
-        age60.setOnClickListener(glassBowl)
-        age70.setOnClickListener(glassBowl)
-        age80.setOnClickListener(glassBowl)
-        age90.setOnClickListener(glassBowl)
-        age100.setOnClickListener(glassBowl)
-
-
-
-
-
-
-
-        tryAgainButton.setOnClickListener {
-
-            firstZodiacMessage.visibility = View.GONE
-            secondZodiacMessage.visibility = View.GONE
-            playSound(R.raw.daysalive_sound_return)
-            // Reset input fields
-            monthField.text.clear()
-            yearField.text.clear()
-            dayField.text.clear()
-
-            // Restore visibility
-            promptTextView.visibility = View.VISIBLE
-            monthField.visibility = View.VISIBLE
-            yearField.visibility = View.VISIBLE
-            dayField.visibility = View.VISIBLE
-            calculateButton.visibility = View.VISIBLE
-
-            tryAgainButton.visibility = View.GONE
-            resultTextViewYears.visibility = View.GONE
-            resultTextViewMessage.visibility = View.GONE
-
-            planetMars.visibility = View.GONE
-            planetVenus.visibility = View.GONE
-            planetMercury.visibility = View.GONE
-            planetTheMoon.visibility = View.GONE
-            planetTheSun.visibility = View.GONE
-            planetMercury.visibility = View.GONE
-            planetVenus.visibility = View.GONE
-            planetPluto.visibility = View.GONE
-            planetJupiter.visibility = View.GONE
-            planetSaturn.visibility = View.GONE
-            planetUranus.visibility = View.GONE
-            planetNeptune.visibility = View.GONE
-
-            age0.visibility = View.GONE
-            age10.visibility = View.GONE
-            age20.visibility = View.GONE
-            age30.visibility = View.GONE
-            age40.visibility = View.GONE
-            age50.visibility = View.GONE
-            age60.visibility = View.GONE
-            age70.visibility = View.GONE
-            age80.visibility = View.GONE
-            age90.visibility = View.GONE
-            age100.visibility = View.GONE
-
-
-        }
-    }
-}
